9c8bf9c240a49cef94dbf252033752e54729fc6b
d-shimizu <shimizu.daisuke.sd@gmail.com>
2019-09-21 23:45:19 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,28 +10,28 @@
  */
 ?>
 
-        </div><!-- .site-content -->
+	</div><!-- .site-content -->
 
-        <footer id="colophon" class="site-footer" role="contentinfo">
-                <div class="site-info">
-                        <?php
-                                /**
-                                 * Fires before the Twenty Fifteen footer text for footer customization.
-                                 *
-                                 * @since Twenty Fifteen 1.0
-                                 */
-                                do_action( 'twentyfifteen_credits' );
-                        ?>
-                        <?php
-                        if ( function_exists( 'the_privacy_policy_link' ) ) {
-                                the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-                        }
-                        ?>
-                        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" class="imprint">
-                                <?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?>
-                        </a>
-                </div><!-- .site-info -->
-        </footer><!-- .site-footer -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php
+				/**
+				 * Fires before the Twenty Fifteen footer text for footer customization.
+				 *
+				 * @since Twenty Fifteen 1.0
+				 */
+				do_action( 'twentyfifteen_credits' );
+			?>
+			<?php
+			if ( function_exists( 'the_privacy_policy_link' ) ) {
+				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+			}
+			?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" class="imprint">
+				<?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?>
+			</a>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
 
